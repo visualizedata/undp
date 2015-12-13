e7ff0df9908c1ea8cce3643be1ed1c1cd4901d8a
Daniel Sauter <daniel@ket.ai>
2015-12-13 20:26:53 +0000

    adding automatic index and scroll

--- web/js/custom.js
+++ web/js/custom.js
@@ -11,6 +11,19 @@
             $("#colors").slideToggle();
         });
         
+        $('li a').bind('click', function(event) {
+			var $anchor = $(this);
+			var nav = $($anchor.attr('href'));
+			if (nav.length) {
+				$('html, body').stop().animate({
+					scrollTop: $($anchor.attr('href')).offset().top
+				}, 500, 'easeInOutExpo', function() {
+
+				});
+				event.preventDefault();
+			}
+		});
+        
     });
 
 })(jQuery);
